b18b4d0f699d2805e148af64667b369f32690646
AILabs <mega4alik@gmail.com>
2025-07-09 23:52:29 -0700

    llama_rl update

--- llama_rl.py
+++ llama_rl.py
@@ -1,7 +1,7 @@
 # venv: US1 asr3.12
 # landing RL training locally with WER reward
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from trl import AutoModelForCausalLMWithValueHead, GRPOTrainer, GRPOConfig
 from datasets import Dataset
 import evaluate
@@ -11,12 +11,12 @@ from webapi_data import prepare_data
 
 def preprocess(sample):
 	#global labels_map
-	messages =  sample["messages"]	 #[{"role":"system", "content":gp}]
-	prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+	messages = sample["messages"] #[{"role":"system", "content":gp}] + 
+	prompt = str(messages) #tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 	#labels_map[prompt[-1000:]] = sample["label"]
 	#encoded = tokenizer(prompt, truncation=True, padding="max_length", max_length=4000, return_tensors="pt")
 	#return {"input_ids": encoded["input_ids"][0], "attention_mask": encoded["attention_mask"][0], "label": sample["label"]}
-	return {"prompt":prompt} #
+	return {"prompt":prompt}
 
 def wer_reward(reference: str, generated: str) -> float:
 	wer_score = wer_metric.compute(predictions=[generated], references=[reference])
@@ -34,14 +34,14 @@ def reward_fn(prompts, completions, completion_ids):
 # ------------------------------
 mode = 1 #1-train, 2-test
 print("Loading model and tokenizer...")
-model_name = "Qwen/Qwen2-0.5B-Instruct"
+model_name = "gpt2" #"Qwen/Qwen2-0.5B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.truncation = 'left'
 wer_metric = evaluate.load("wer")
 
-#base_model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
-#model = AutoModelForCausalLMWithValueHead.from_pretrained(base_model, torch_dtype=torch.float16)
+#base_model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True) #, torch_dtype=torch.float16
+#model = AutoModelForCausalLMWithValueHead.from_pretrained(base_model)
 
 # --------- Dataset -------------
 d, gp = prepare_data(mode)
--- run.sh
+++ run.sh
@@ -1,25 +1,5 @@
-"""
-curl -X POST https://anuarsh--rl-reward-web-inference.modal.run -H "Content-Type: application/json" -d '{"messages":[{"role":"user","content":"What is the price?"}, {"role":"assistant","content":"it is based on service usage from 2.99 to 4 USD"}, {"role": "user", "content": "On a scale from 1 to 5, how correct and helpful was the assistants last response based on history of conversation? Only return a single number. Ex, 4"} ]}'
-
-
-curl -X POST https://anuarsh--rl-reward-web-inference.modal.run -H "Content-Type: application/json" -d '{"messages":[
-{"role":"user","content":"What is the price?"}, 
-{"role":"assistant","content":"it is based on service usage from 2.99 to 4 USD"}, 
-{"role": "user", "content": "On a scale from 1 to 5, how correct and helpful was the assistants last response based on history of conversation? Only return a single number. Ex, 4"} ]}'
-
-
-# gives 5
 curl -X POST https://anuarsh--rl-reward-web-inference.modal.run -H "Content-Type: application/json" -d '{"messages":[
 {"role":"assistant","content":"Hi, I am a smart chatbot. I can understand the context and respond in various ways. Thank you for your interest. Would you like to automate your conversations with your leads?"},
 {"role":"user","content":"Yes, try webapi.ai for free"},
-{"role":"assistant","content":"Awesome, may I know what business are you in?"},
-{"role": "user", "content": "Task: On a scale from 1 to 5, how correct was the last assistant response? Only return a single number. Ex, 4"} ]}'
-
-
-"""
-
-curl -X POST https://anuarsh--rl-reward-web-inference.modal.run -H "Content-Type: application/json" -d '{"messages":[
-{"role":"assistant","content":"Hi, I am a smart chatbot. I can understand the context and respond in various ways. Thank you for your interest. Would you like to automate your conversations with your leads?"},
-{"role":"user","content":"Yes, try webapi.ai for free"},
-{"role":"assistant","content":"Great, may I know what business you are in?"},
-{"role": "user", "content": "Task: Is the last assistant response correct? Just answer Yes or No"} ]}'
+{"role":"assistant","content":"Awesome, may I know what business you are in?"},
+{"role": "user", "content": "Is the last assistant response correct? Just answer Yes or No"} ]}'
